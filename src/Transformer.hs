509cdcf70931f740bff73187b7d622f4bc0ebef2
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2019-04-11 00:13:29 -0400

    Add currency function

--- src/Transformer.hs
+++ src/Transformer.hs
@@ -8,12 +8,25 @@ module Transformer
 
 import Config
 import Control.Monad.Reader
+import CoinberryApi as CA
 
 type App = ReaderT Context IO
+
+
+getCurrency :: String -> CA.Currency
+getCurrency "CAD" = CAD
+getCurrency "BTC" = BTC
+getCurrency "ETH" = ETH
+getCurrency "LTC" = LTC
+getCurrency "XRP" = XRP
 
 -- | Function to be implemented.
 printPrice :: App ()
 printPrice = error "please implement printPrice to fetch currency price"
+-- printPrice = do
+--     ctx <- ask
+--     priceToPrint <- CA.price $ getCurrency $ currency (ctxConfig ctx)
+--     liftIO $ ctxPrint ctx $ show $ priceToPrint
 
 -- | Using the configuration and print function from Context will print the Currency
 printCurrency :: App ()
